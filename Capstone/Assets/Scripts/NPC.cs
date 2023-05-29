d963ff5ad9116495d0d75f35bf19125d34328e85
Hunter Smith <hunteralansmith@outlook.com>
2023-05-29 15:37:58 -0700

    UI testing

--- Capstone/Assets/Scripts/NPC.cs
+++ Capstone/Assets/Scripts/NPC.cs
@@ -2,12 +2,15 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class NPC : MonoBehaviour
 {
     private Texture2D cursorTexture;
     private CursorMode cursorMode = CursorMode.Auto;
     private Vector2 hotSpot = Vector2.zero;
+
+    [SerializeField] private TextMeshProUGUI outputText;
 
     // Start is called before the first frame update
     void Start()
@@ -23,10 +26,11 @@ public class NPC : MonoBehaviour
 
     private void OnMouseOver()
     {
-        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+        //Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
         if (Input.GetMouseButtonDown(1)) { 
             Debug.Log("Click");
             //Write to dialogue bar in UI
+            outputText.text = "Sample Text! Although this is probably way too long to fit in the box!";
         }
     }
 
