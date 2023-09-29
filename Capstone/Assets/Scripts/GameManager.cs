70c9a2486408366d68d947f236ab0757c360b353
Peamupbubber <hunteralansmith@outlook.com>
2023-09-28 17:16:05 -0700

    some scene transitions

--- Capstone/Assets/Scripts/GameManager.cs
+++ Capstone/Assets/Scripts/GameManager.cs
@@ -1,13 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    List<AsyncOperation> scenesToLoad = new List<AsyncOperation>();
+
+    [SerializeField] private TMP_Text errorDisplayText;
+
+    [SerializeField] private GameObject player;
+    private PlayerInfo playerInfo;
+
+    private void Awake()
+    {
+        playerInfo = player.GetComponent<PlayerInfo>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
@@ -16,7 +30,12 @@ public class GameManager : MonoBehaviour
         
     }
 
-    public void StartGame() { 
-        
+    public void StartGame() {
+        if (playerInfo.playerName == "" || playerInfo.numPronouns == 0)
+        {
+            errorDisplayText.text = "Player must have a name and at least one set of pronouns";
+        }
+        else
+            scenesToLoad.Add(SceneManager.LoadSceneAsync("SampleScene"));
     }
 }
--- Capstone/Assets/Testing/Test Scripts/Test.cs
+++ Capstone/Assets/Testing/Test Scripts/Test.cs
@@ -8,6 +8,8 @@ public class Test : MonoBehaviour
 
     public Animator animator;
 
+    bool facingRight = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,16 +19,30 @@ public class Test : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKey(KeyCode.D))
         {
+            if (!facingRight)
+            {
+                Vector3 currentScale = gameObject.transform.localScale;
+                currentScale.x *= -1;
+                gameObject.transform.localScale = currentScale;
+                facingRight = true;
+            }
             animator.SetTrigger("Move");
         }
-        else if (Input.GetKeyDown(KeyCode.A)) {
-            //transform.rotation.y = 180;
-            Vector3 currentScale = gameObject.transform.localScale;
-            currentScale.x *= -1;
-            gameObject.transform.localScale = currentScale;
+        else if (Input.GetKey(KeyCode.A))
+        {
+            if (facingRight)
+            {
+                Vector3 currentScale = gameObject.transform.localScale;
+                currentScale.x *= -1;
+                gameObject.transform.localScale = currentScale;
+                facingRight = false;
+            }
             animator.SetTrigger("Move");
+        }
+        else {
+            animator.SetTrigger("Stop Moving");
         }
     }
 }
